513a51b415a5d81080aaa763ba21601aa22b5ca9
jaimittal <jai_mittal24@milton.edu>
2023-02-07 11:26:56 -0500

    this is my aquarium game.

--- src/Astronaut.java
+++ src/Astronaut.java
@@ -1,2 +1,92 @@
-package PACKAGE_NAME;public class Astronaut {
+import java.awt.*;
+
+/**
+ * Created by chales on 11/6/2017.
+ */
+public class Astronaut {
+
+    //VARIABLE DECLARATION SECTION
+    //Here's where you state which variables you are going to use.
+    public String name;                //holds the name of the hero
+    public int xpos;                //the x position
+    public int ypos;                //the y position
+    public int dx;                    //the speed of the hero in the x direction
+    public int dy;                    //the speed of the hero in the y direction
+    public int width;
+    public int height;
+    public boolean isAlive;            //a boolean to denote if the hero is alive or dead.
+    public Rectangle rec;
+    public double lives;
+    public double maxHealth;
+
+    // METHOD DEFINITION SECTION
+
+    // Constructor Definition
+    // A constructor builds the object when called and sets variable values.
+
+
+    //This is a SECOND constructor that takes 3 parameters.  This allows us to specify the hero's name and position when we build it.
+    // if you put in a String, an int and an int the program will use this constructor instead of the one above.
+    public Astronaut(int pXpos, int pYpos) {
+        xpos = pXpos;
+        ypos = pYpos;
+        dx = 5;
+        dy = 0;
+        width = 110;
+        height = 110;
+        isAlive = true;
+        rec = new Rectangle (xpos, ypos, height, width);
+        lives= 10;
+        maxHealth=20;
+
+    } // constructor
+
+    //The move method.  Everytime this is run (or "called") the hero's x position and y position change by dx and dy
+    public void move() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rec = new Rectangle (xpos, ypos, height, width);
+    }
+//    public Astronaut(int pXpos, int pYpos) {
+//        xpos = pXpos;
+//        ypos = pYpos;
+//        dx =10;
+//        dy =10;
+//        width = 60;
+//        height = 60;
+//        isAlive = true;
+
+    public void wrap() {
+        if (xpos > 1000) {
+            xpos = 0;
+        }
+        if (ypos > 1000) {
+            ypos = 0;
+        }
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rec = new Rectangle (xpos, ypos, height, width);
+    }
+
+
+    public void bounce() {
+        if (xpos > 1000) {
+            dx = -dx;
+        }
+        if (ypos > 700) {
+            dy = -dy;
+        }
+        if (xpos < 0) {
+            dx = -dx;
+        }
+        if (ypos < 0) {
+            dy = -dy;
+        }
+
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rec = new Rectangle (xpos, ypos, height, width);
+
+    }
 }
+
--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -1,2 +1,262 @@
-package PACKAGE_NAME;public class BasicGameApp {
+//Basic Game Application
+//Version 2
+// Basic Object, Image, Movement
+// Astronaut moves to the right.
+// Threaded
+
+//K. Chun 8/2018
+
+//*******************************************************************************
+//Import Section
+//Add Java libraries needed for the game
+//import java.awt.Canvas;
+
+//Graphics Libraries
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+//*******************************************************************************
+// Class Definition Section
+
+public class BasicGameApp implements Runnable {
+
+    //Variable Definition Section
+    //Declare the variables used in the program
+    //You can set their initial values too
+
+    //Sets the width and height of the program window
+    final int WIDTH = 1000;
+    final int HEIGHT = 700;
+
+    //Declare the variables needed for the graphics
+    public JFrame frame;
+    public Canvas canvas;
+    public JPanel panel;
+
+    public BufferStrategy bufferStrategy;
+    public Image ConorPic;
+    public Image ringPic;
+    public Image israelPic;
+    public Image refereePic;
+    public Image winPic;
+
+    //Declare the objects used in the program
+    //These are things that are made up of more than one variable type
+    private Astronaut Conor;
+    private Astronaut israel;
+
+    private Astronaut referee;
+    public int counter=0;
+
+    public boolean winning;
+
+
+
+    // Main method definition
+    // This is the code that runs first and automatically
+    public static void main(String[] args) {
+        BasicGameApp ex = new BasicGameApp();   //creates a new instance of the game
+        new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method
+    }
+
+
+    // Constructor Method
+    // This has the same name as the class
+    // This section is the setup portion of the program
+    // Initialize your variables and construct your program objects here.
+    public BasicGameApp() {
+
+        setUpGraphics();
+
+        //variable and objects
+        //create (construct) the objects needed for the game and load up
+        ConorPic = Toolkit.getDefaultToolkit().getImage("Conor.jpeg");
+        ringPic = Toolkit.getDefaultToolkit().getImage("ring.png");//load the picture
+        israelPic = Toolkit.getDefaultToolkit().getImage("israel.jpeg");
+        refereePic = Toolkit.getDefaultToolkit().getImage("referee.jpeg");
+        winPic=Toolkit.getDefaultToolkit().getImage("win.jpeg");
+        winning=false;
+        
+        Conor = new Astronaut(120, 240);
+        israel = new Astronaut(200, 420);
+        referee = new Astronaut(0, 0);
+        israel.dy=-10;
+        israel.dx=10;
+        Conor.dy=-10;
+        Conor.dx=10;
+
+
+
+    }// BasicGameApp()
+
+
+//*******************************************************************************
+//User Method Section
+//
+// put your code to do things here.
+
+    // main thread
+    // this is the code that plays the game after you set things up
+    public void run() {
+
+        //for the moment we will loop things forever.
+        while (true) {
+
+            moveThings();  //move all the game objects
+            render();  // paint the graphics
+            pause(20); // sleep for 10 ms
+        }
+    }
+    public void change()
+    {
+        if(Conor.rec.intersects(israel.rec));
+
+        System.out. println("crash");
+        Conor.dx = 1*Conor.dx;
+        Conor.dy = -Conor.dy;
+        israel.dx = 1*israel.dx;
+        israel.dy = -israel.dy;
+    }
+
+
+    public void crash()
+    {
+        if(Conor.rec.intersects(israel.rec) &&israel.isAlive == true &&Conor.isAlive == true)
+        {
+            System.out. println("crash");
+            Conor.dx = 1*Conor.dx;
+            Conor.dy = -Conor.dy;
+            israel.dx = 1*israel.dx;
+            israel.dy = -israel.dy;
+            //israel.isAlive = false;
+            counter++;
+            Conor.lives--;
+        }
+//        if(counter==10){
+//            israel.isAlive=false;
+//        }
+        if(Conor.rec.intersects(referee.rec) &&referee.isAlive == true &&Conor.isAlive == true)
+        {
+            System.out. println("crash");
+            Conor.dx = 1*Conor.dx;
+            Conor.dy = -Conor.dy;
+            referee.dx = 1*referee.dx;
+            referee.dy = -referee.dy;
+          //  referee.isAlive = false;
+            counter++;
+            Conor.lives++;
+        }
+        if(Conor.lives==0){
+            Conor.isAlive=false;
+            winning=true;
+        }
+
+    }
+    public void size()
+    {
+        if(Conor.rec.intersects(israel.rec))
+        {
+            Conor.height = 3*Conor.height;
+            Conor.width = 3*Conor.width;
+            israel.height = 3*israel.height;
+            israel.width = 3*israel.width;
+        }
+    }
+
+    public void moveThings()
+    {
+        //calls the move( ) code in the objects
+        Conor.bounce();
+        israel.bounce();
+        referee.wrap();
+        crash();
+
+
+    }
+
+    //Pauses or sleeps the computer for the amount specified in milliseconds
+    public void pause(int time ){
+        //sleep
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    //Graphics setup method
+    private void setUpGraphics() {
+        frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
+
+        panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+        panel.setLayout(null);   //set the layout
+
+        // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+        // and trap input events (Mouse and Keyboard events)
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, WIDTH, HEIGHT);
+        canvas.setIgnoreRepaint(true);
+
+        panel.add(canvas);  // adds the canvas to the panel.
+
+        // frame operations
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+        frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+        frame.setResizable(false);   //makes it so the frame cannot be resized
+        frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+        // sets up things so the screen displays images nicely.
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
+        canvas.requestFocus();
+        System.out.println("DONE graphic setup");
+
+    }
+
+
+    //paints things on the screen using bufferStrategy
+    private void render() {
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+
+        if(winning == false){
+
+
+        //draw the image of the astronaut
+        g.drawImage(ringPic, 0, 0, WIDTH, HEIGHT, null);
+        if (Conor.isAlive == true) {
+            g.drawImage(ConorPic, Conor.xpos, Conor.ypos, Conor.width, Conor.height, null);
+            g.draw(new Rectangle(Conor.xpos, Conor.ypos, Conor.width, Conor.height));
+        }
+        g.setColor(Color.red);
+        g.fillRect(Conor.xpos, Conor.ypos - 10, Conor.width, 10);
+        g.setColor(Color.green);
+
+        double calc = (Conor.lives / Conor.maxHealth);
+
+        System.out.println(calc);
+        // System.out.println(Conor.maxHealth);
+
+        //   double calc = (Conor.lives/Conor.maxHealth);
+        g.fillRect(Conor.xpos, Conor.ypos - 10, (int) (Conor.width * (Conor.lives / Conor.maxHealth)), 10);
+
+        g.drawImage(israelPic, israel.xpos, israel.ypos, israel.width, israel.height, null);
+        g.drawImage(refereePic, referee.xpos, referee.ypos, referee.width, referee.height, null);
+
+        g.draw(new Rectangle(israel.xpos, israel.ypos, israel.width, israel.height));
+        g.draw(new Rectangle(referee.xpos, referee.ypos, referee.width, referee.height));
+    }
+        else{
+            g.drawImage(winPic, 0, 0, WIDTH, HEIGHT, null);
+        }
+
+        g.dispose();
+
+        bufferStrategy.show();
+    }
 }
