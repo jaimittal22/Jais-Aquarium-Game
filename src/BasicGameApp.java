bdc46e11f0694dddf3ff99e0d153e979ad4cea3f
jaimittal <jai_mittal24@milton.edu>
2024-03-08 13:23:06 -0500

    API

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -20,9 +20,21 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferStrategy;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.event.MouseInputListener;
+import java.text.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 //*******************************************************************************
@@ -340,6 +352,92 @@ referee.dx=0;
 
     @Override
     public void mouseMoved(MouseEvent e) {
+
+    }
+    public  void pull() throws ParseException {
+        String output = "abc";
+        String totlaJson="";
+        try {
+
+            URL url = new URL("https://last-airbender-api.fly.dev/api/v1/characters");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+                totlaJson+=output;
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JSONParser parser = new JSONParser();
+        //System.out.println(str);
+        jsonObject = (org.json.simple.JSONArray) parser.parse(totlaJson);
+        System.out.println(jsonObject);
+//System.out.println("hi");
+        try {
+            for(int j = 0; j<jsonObject.size(); j++){
+                System.out.println(jsonObject.get(j));
+                JSONObject secretTunnelGuy = (JSONObject) jsonObject.get(j);
+                System.out.println(secretTunnelGuy.get("name"));
+                JSONArray ally1 = (JSONArray) secretTunnelGuy.get("allies");
+                System.out.println(ally1.get(0));
+            }
+
+
+
+
+
+
+            //   System.out.println(jsonObject.get(1));
+            //   JSONObject guy2 = (JSONObject) jsonObject.get(1);
+            //   System.out.println(guy2.get("name"));
+            //   JSONArray ally2 = (JSONArray) guy2.get("allies");
+            //   System.out.println(ally2.get(0));
+
+
+//            for(jsonObject.size()){
+//
+//            }
+//
+//            String name = (String)jsonObject.get("name");
+//
+//            org.json.simple.JSONArray msg = (org.json.simple.JSONArray) jsonObject.get("starships");
+//            int n =   msg.size(); //(msg).length();
+//            for (int i = 0; i < n; ++i) {
+//                String test =(String) msg.get(i);
+//                System.out.println(test);
+//                // System.out.println(person.getInt("key"));
+//            }
+//         //   String name= (String)jsonObject.get("eye_color");
+//            System.out.println(name);
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
 
     }
 }
